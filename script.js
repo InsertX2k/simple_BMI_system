a07f7d7443a0d0c4331dc47ee2cb73500158cb24
Ziad <ziad.ahmed7881@gmail.com>
2026-05-07 16:26:13 +0300

    Update to v2.0
    * Added some UI Animations
    * Added an age-check
    * Added a color-coded weight category.
    * Added imperial measurement system.
    
    Signed-off-by: Ziad <ziad.ahmed7881@gmail.com>

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const INTL_DATETIMEFORMAT_OPTIONS = {
     second:"numeric"
 };
 const INTL_DATETIMEFORMAT_LOCALE = 'en-US';
-
+const MEASURMENT_SYS_METRIC = 1;
+const MEASURMENT_SYS_IMPERIAL = 2;
+const MEASURMENT_SYS_NULL = 0;
 
 
 var calcBtn = null;
@@ -23,12 +25,29 @@ var weightInput = null;
 var heightInput = null;
 var hs_table_contents = null;
 var clr_all_btn = null;
+var age_chk_confirm_btn = null;
+var measurment_sys_radios = null;
+
+// global variable that holds the current measurment system
+var current_measurment_system = MEASURMENT_SYS_METRIC;
 
 // db connection object
 var dbConnection = null;
 
 // 2D Array of BMI Values history
 var hs = null;
+
+// categories elements
+var underweight_cat = null;
+var normal_cat = null;
+var overweight_cat = null;
+var obese_cat = null;
+
+// categories elements titles
+var underweight_cat_title = null;
+var normal_cat_title = null;
+var overweight_cat_title = null;
+var obese_cat_title = null;
 
 
 
@@ -40,7 +59,23 @@ function calculateBMI(params) {
     var heightInMeters = heightInput.value / 100;
     console.log("debug: height in meters is: " + heightInMeters);
     // calculate the BMI
-    var BMI_Value = (weightInput.value / (heightInMeters**2));
+    // here we have two methods to calculate the BMI according to which 
+    // measurment system we are in
+    /*
+        For metric system:
+            weight / (height in meters**2)
+        For imperial system:
+            (weight / (height**2) ) * 703
+        
+    */
+    // metric system BMI value
+    if (current_measurment_system === MEASURMENT_SYS_METRIC) {
+        var BMI_Value = Math.round((weightInput.value / (heightInMeters**2)));
+    } else if (current_measurment_system === MEASURMENT_SYS_IMPERIAL) {
+        var BMI_Value = Math.round( (weightInput.value / (heightInput.value**2)) * 703 );
+    } else {
+        var BMI_Value = -1;
+    }
     console.log("debug BMI Value: " + BMI_Value);
     // display the BMI Value to the user.
     bmiValDisplay.innerHTML = BMI_Value;
@@ -51,14 +86,33 @@ function calculateBMI(params) {
         Overweight: 25 – 29.9
         Obese: BMI ≥ 30
      */
+    // delete style attributes for all weight categories divs
+    underweight_cat.removeAttribute("style");
+    normal_cat.removeAttribute("style");
+    overweight_cat.removeAttribute("style");
+    obese_cat.removeAttribute("style");
+    // remove class vtext from all weight categories descriptions if exist.
+    document.querySelectorAll(".weight_category_title").forEach(elem => {
+        elem.classList.remove("vtext");
+    });
+
     if (BMI_Value < 18.5) {
         bmiValDesc.innerHTML = "Underweight";
+        underweight_cat.style = "background-color: purple;";
+        underweight_cat_title.classList.add("vtext");
     } else if (BMI_Value >= 18.5 && BMI_Value < 25) {
         bmiValDesc.innerHTML = "Normal";
+        normal_cat.style = "background-color: green;";
+        normal_cat_title.classList.add("vtext");
     } else if (BMI_Value >= 25 && BMI_Value < 30) {
         bmiValDesc.innerHTML = "Overweight";
+        overweight_cat.style = "background-color: yellow;";
+        overweight_cat_title.classList.add("vtext");
+        overweight_cat_title.style = "color: black;";
     } else {
         bmiValDesc.innerHTML = "Obese";
+        obese_cat.style = "background-color: red;";
+        obese_cat_title.classList.add("vtext");
     };
     // okay so here should reside the code for storing the calculated value
     // with the current date into the indexedDB
@@ -191,6 +245,51 @@ function onClrAllBtnClick(evt) {
     }
 }
 
+function onAgeCheckConfirm(evt) {
+    // change our background color to something valid.
+    age_chk_confirm_btn.style = "background-color:grey;";
+    // we must remove the classes blur and noclicks from the body
+    document.body.removeAttribute("class");
+    // hide our full-screen overlay
+    document.getElementById("age_check_overlay").style.display = "none";
+    return;
+}
+
+/*
+    A callback function for when the measurment system changes (by the click of a radio button)
+
+    For the metric (SI) system:
+        * Weight: Kilograms
+        * Height: centimeters
+    For the imperial (US) system:
+        * Weight: pounds
+        * Height: inches
+
+*/
+function onSysChange(params) {
+    console.log("DEBUG: onSysChange() called!");
+    // update the global variable according to the currently selected system unit
+    var _curMSysVal = document.querySelector('input[name="sys"]:checked').value;
+    if (_curMSysVal === "metric") {
+        current_measurment_system = MEASURMENT_SYS_METRIC;
+        console.log("DEBUG: Currently selected measurment system is Metric!");
+        // update displayed weight and height hints to kilograms and centimeters
+        document.getElementById("weight_unit_title").innerHTML = "Kilograms";
+        document.getElementById("height_unit_title").innerHTML = "centimeters";
+    } else if (_curMSysVal === "imperial") {
+        current_measurment_system = MEASURMENT_SYS_IMPERIAL;
+        console.log("DEBUG: currently selected measurment system is: imperial");
+        document.getElementById("weight_unit_title").innerHTML = "Pounds (lbs)";
+        document.getElementById("height_unit_title").innerHTML = "inches";
+    } else {
+        current_measurment_system = MEASURMENT_SYS_NULL;
+        console.log("ERROR: Invalid measurment system selected!");
+        document.getElementById("weight_unit_title").innerHTML = "Invalid weight unit!";
+        document.getElementById("height_unit_title").innerHTML = "Invalid height unit!";
+    }
+    // then update the displayed weight and height hints
+    return;
+}
 
 
 function onLoad(params) {
@@ -203,6 +302,24 @@ function onLoad(params) {
     heightInput = document.getElementById("height_input");
     hs_table_contents = document.getElementById("history_table_contents");
     clr_all_btn = document.getElementById("clear_all_btn");
+    age_chk_confirm_btn = document.getElementById("age_check_confirm_btn");
+    measurment_sys_radios = document.querySelectorAll('input[name="sys"]');
+    // weight categories
+    underweight_cat = document.getElementById("underweight_category");
+    normal_cat = document.getElementById("normal_category");
+    overweight_cat = document.getElementById("overweight_category");
+    obese_cat = document.getElementById("obese_category");
+    // weight categories titles
+    underweight_cat_title = document.getElementById("uw_cat_title");
+    normal_cat_title = document.getElementById("normal_cat_title");
+    overweight_cat_title = document.getElementById("ow_cat_title");
+    obese_cat_title = document.getElementById("obese_cat_title");
+    // hook the callback function onSysChange to all the radios in sys radio group
+    measurment_sys_radios.forEach((radio, k, p) => {
+        radio.addEventListener('change', onSysChange);
+    });
+    // bind the age check confirmation button to its function
+    age_chk_confirm_btn.addEventListener("click", onAgeCheckConfirm);
     // hook calcBtn onClick event to calcBMI function
     calcBtn.addEventListener("click", calculateBMI);
     // hook onClrAllBtnClick 
